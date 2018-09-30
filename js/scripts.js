01105d2522f28fb89faf0c2b0d0d43ef056ebb75
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-30 09:31:04 -0700

    create if else to display error if only numbers are input and if no sentence is input, change text to red for these errors

--- js/scripts.js
+++ js/scripts.js
@@ -7,33 +7,39 @@ $(document).ready(function(){
 
     //collect user input and push to "input" array
     var inputString = $("#sentence").val();
+    if (parseInt(inputString) >= 0) {
+      $("#output").text("I can't provide a word puzzle from just numbers! Please input a new sentence and try again.");
+      $("p").addClass("error");
+    } else {
+      //chop sentence into array of each character
+      var inputArrayTemp = inputString.split("");
+      inputArrayTemp.forEach(function(character){
+        inputArray.push(character);
+      });
 
-    //chop sentence into array of each character
-    var inputArrayTemp = inputString.split("");
-    inputArrayTemp.forEach(function(character){
-      inputArray.push(character);
-    })
+      //create loop that executes for each letter of sentence
+      for (var index = 0; index < inputArray.length; index +=1 ) {
+          if (inputArray[index] === "a" || inputArray[index] === "e" || inputArray[index] === "i" || inputArray[index] === "o" || inputArray[index] === "u" || inputArray[index] === "A" || inputArray[index] === "E" || inputArray[index] === "I" || inputArray[index] === "O" || inputArray[index] === "U") {
+          inputArray[index] = "-";
+          console.log("replacing vowel");
+        } else {;
+          console.log("not a vowel");
+        }
+      };
 
-    //create loop that executes for each letter of sentence
-    for (var index = 0; index < inputArray.length; index +=1 ) {
-        if (inputArray[index] === "a" || inputArray[index] === "e" || inputArray[index] === "i" || inputArray[index] === "o" || inputArray[index] === "u" || inputArray[index] === "A" || inputArray[index] === "E" || inputArray[index] === "I" || inputArray[index] === "O" || inputArray[index] === "U") {
-        inputArray[index] = "-";
-        console.log("replacing vowel");
-      } else {;
-        console.log("not a vowel");
+      //output the initial input, but with vowels replaced/
+      var outputString = inputArray.join("");
+      if (outputString.includes("-")) {
+      $("#output").text("Your puzzle is: " + outputString);
+      } else if (outputString == "") {
+        $("#output").text("No starting sentence was entered. Please try again.");
+        $("p").addClass("error");
+      } else {
+        $("#output").text("Please enter a different sentence -- this one is too easy to guess!");
+        $("p").addClass("error");
       }
-    };
-
-    //output the initial input, buw with vowels replaced/
-    var outputString = inputArray.join("");
-    if (outputString.includes("-")) {
-    $("#output").text(outputString);
-  } else {
-    $("#output").text("Please enter a different sentence -- this one is too easy to guess!");
-  }
-
+    }
     $("#scrambled").show();
-
   });
 
 });
