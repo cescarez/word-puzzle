04332ef4b37f29af8ae825c1a4de0c4411d90bc6
Christabel Escarez <christabel.escarez@gmail.com>
2018-09-30 07:32:45 -0700

    change initial input from array to string

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-var vowels = ["a", "e", "i", "o", "u"];
-var input = [];
-var inputArray = [];
+// var inputArray = [];
 
 $(document).ready(function(){
   $("form").submit(function(event){
@@ -8,29 +6,30 @@ $(document).ready(function(){
     $("#puzzle").hide();
 
     //collect user input and push to "input" array
-    input.push($("#sentence").val());
+    var inputString = $("#sentence").val();
 
     //chop sentence into array of each character
-    input.forEach(function(sentence){
-      inputArray.push(sentence.split(""));
-    });
+    var inputArray = inputString.split("");
+    // var inputArray = input.map(function(sentence){
+    //   input.split("");
+    // });
+    // input.forEach(function(sentence){
+    //   inputArray.push(sentence.split(""));
+    // });
 
     console.log(inputArray);
 
     //create loop that executes for each letter of sentence
     for (var index = 0; index < inputArray.length; index +=1 ) {
 
-      // if (inputArray[index.length] === "a" || inputArray[index] === "e" || inputArray[index] === "i" || inputArray[index] === "o" || inputArray[index] === "u") {
-      //   var replacedInput = inputArray[index].replace("-");
-      //   $("#output").text(replacedInput);
-      //   console.log(replacedInput);
-      // } else if {
-      //   $("#output").text("Please enter a different sentence -- this one is too easy to guess!")
-      //   console.log("no vowelz");
-      // }
-      // if (inputArray[index].val();==="a") {
-      //   alert("working");
-      // };
+      if (inputArray[$(index).val()] === "a" || inputArray[index] === "e" || inputArray[index] === "i" || inputArray[index] === "o" || inputArray[index] === "u") {
+        var replacedInput = inputArray[index].replace("-");
+        $("#output").text(replacedInput);
+        console.log(replacedInput);
+      } else {
+        $("#output").text("Please enter a different sentence -- this one is too easy to guess!");
+        console.log("no vowelz");
+      }
 
 
       var inputIterator = inputArray.values();
